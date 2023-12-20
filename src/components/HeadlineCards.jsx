eb4d7da1d927c13aa18e0844bac6783bfa45f482
luttinno <luka.grdzelishvili1994@gmail.com>
2023-12-20 17:17:52 +0400

    feat:first

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const HeadlineCards = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/*Card*/}
+      <div className="rounded-xl relative">
+        {/*overlay*/}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1668094688280-cc63bb9ff1b6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="/"
+        />
+      </div>
+      {/*Card*/}
+      <div className="rounded-xl relative">
+        {/*overlay*/}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restourants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1687686515394-5f5b7d50b01b?q=80&w=1111&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="/"
+        />
+      </div>
+      {/*Card*/}
+      <div className="rounded-xl relative">
+        {/*overlay*/}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
+          <p className="px-2">Tasty Treats</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://plus.unsplash.com/premium_photo-1661310177352-f586bf23a403?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
